21f0cdfbc8f54a2f90431e1fd8175add21e9f669
anvesha-07 <sahuanvesha7004@gmail.com>
2026-08-31 13:10:38 +0530

    Time: 5 ms (82.17%), Space: 46 MB (52.74%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }else{
+            int rev = 0;
+            int original = x;
+            while(x!=0){
+                int  div = x % 10;
+                 x = x/10;
+                rev =  rev * 10 + div;
+            }
+        if(original == rev){
+            return true;
+        }else{
+            return false;
+        }
+        }
+}  
+}
